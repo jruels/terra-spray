213882431f86500c876473200e9c12ed92237cc0
Jason Smith <jason@smithss.org>
2017-11-16 18:54:43 -0800

    update kargo to kubespray

--- 01-create-inventory.tf
+++ 01-create-inventory.tf
@@ -2,30 +2,30 @@ resource "null_resource" "ansible-provision" {
   depends_on = ["aws_instance.aws-k8s-master", "aws_instance.k8s-members"]
 
   provisioner "local-exec" {
-    command = "echo \"[kube-master]\" > kargo/inventory/inventory"
+    command = "echo \"[kube-master]\" > kubespray/inventory/inventory"
   }
 
   provisioner "local-exec" {
-    command = "echo \"${format("%s ansible_ssh_user=%s", aws_instance.aws-k8s-master.0.public_ip, var.ssh_user)}\" >> kargo/inventory/inventory"
+    command = "echo \"${format("%s ansible_ssh_user=%s", aws_instance.aws-k8s-master.0.public_ip, var.ssh_user)}\" >> kubespray/inventory/inventory"
   }
   provisioner "local-exec" {
-    command = "echo \"[etcd]\" >> kargo/inventory/inventory"
-  }
-
-  provisioner "local-exec" {
-    command = "echo \"${format("%s ansible_ssh_user=%s", aws_instance.aws-k8s-master.0.public_ip, var.ssh_user)}\" >> kargo/inventory/inventory"
+    command = "echo \"[etcd]\" >> kubespray/inventory/inventory"
   }
 
   provisioner "local-exec" {
-    command = "echo \"[kube-node]\" >> kargo/inventory/inventory"
+    command = "echo \"${format("%s ansible_ssh_user=%s", aws_instance.aws-k8s-master.0.public_ip, var.ssh_user)}\" >> kubespray/inventory/inventory"
   }
 
   provisioner "local-exec" {
-    command = "echo \"${join("\n",formatlist("%s ansible_ssh_user=%s", aws_instance.k8s-members.*.public_ip, var.ssh_user))}\" >> kargo/inventory/inventory"
+    command = "echo \"[kube-node]\" >> kubespray/inventory/inventory"
   }
 
   provisioner "local-exec" {
-    command =  "echo \"\n[k8s-cluster:children]\nkube-node\nkube-master\" >> kargo/inventory/inventory"
+    command = "echo \"${join("\n",formatlist("%s ansible_ssh_user=%s", aws_instance.k8s-members.*.public_ip, var.ssh_user))}\" >> kubespray/inventory/inventory"
+  }
+
+  provisioner "local-exec" {
+    command =  "echo \"\n[k8s-cluster:children]\nkube-node\nkube-master\" >> kubespray/inventory/inventory"
   }
 
 }
